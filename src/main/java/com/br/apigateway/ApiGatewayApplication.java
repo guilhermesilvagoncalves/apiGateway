395084955e2f5051626b6d7a15c37028844e1500
Guilherme da Silva Gonçalves <guilherme.dasilva@dafiti.com.br>
2021-01-31 22:18:39 -0300

    Code Refactor

--- src/main/java/com/br/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/br/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,6 @@ package com.br.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
